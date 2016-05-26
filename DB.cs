6c605323f1a3f852d5540918bc946ec3a9fff4df
小斌i <milibbs@163.com>
2016-05-26 06:01:55 +0000

    ....
    .......

---
+++ DB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 中超联赛管理系统
+{
+    class DB
+    {
+        public static string constrin = "Data Source=.;Initial Catalog=FootballScheduleManager3;Integrated Security=True";
+    }
+}
